23b43af50b1acab1b0d59fe6363e65d12d3a2fbe
partha7978 <muduliparthasarathi@gmail.com>
2021-12-31 23:54:23 +0530

    redesigning the menu bar

---
+++ scripts/menuBar.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.querySelector(".nav-bar").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.querySelector(".nav-bar").style.width = "0%";
+  }
