b7994050980b0399334720b3db297289ab5d39b9
partha7978 <muduliparthasarathi@gmail.com>
2022-01-02 00:17:01 +0530

    fixed the issue of 1st string not showing

---
+++ scripts/autoText.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".gradient", {
+    strings: ["Partha Sarathi Muduli", "Web Developer", "Software Engineer", "Competitive Coder"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+});
--- scripts/menuBar.js
+++ scripts/menuBar.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.querySelector(".nav-bar").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.querySelector(".nav-bar").style.width = "0%";
-  }
+  document.querySelector(".nav-bar").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.querySelector(".nav-bar").style.width = "0%";
+}
