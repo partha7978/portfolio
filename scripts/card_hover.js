7ed6ba2275778460b9bf428ccb5e7664608563ea
partha7978 <muduliparthasarathi@gmail.com>
2022-01-22 21:05:17 +0530

    adding final text hover to all skills buttons

--- scripts/card_hover.js
+++ scripts/card_hover.js
@@ -28,6 +28,8 @@ cardThree.addEventListener("mouseout", () => {
 
 
 //skills content icons hover
+
+//java
 let skillIcons = document.querySelector(".skillIconImg");
 skillIcons.addEventListener("mouseover", () => {
     document.querySelector(".skillIconText").style.opacity = "1";
@@ -36,3 +38,119 @@ skillIcons.addEventListener("mouseout", () => {
     document.querySelector(".skillIconText").style.opacity = "0";
 });
 
+// c
+let skillIcons2 = document.querySelector(".skillIconImg2");
+skillIcons2.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText2").style.opacity = "1";
+});
+skillIcons2.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText2").style.opacity = "0";
+});
+
+//c++
+let skillIcons3 = document.querySelector(".skillIconImg3");
+skillIcons3.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText3").style.opacity = "1";
+});
+skillIcons3.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText3").style.opacity = "0";
+});
+
+//html
+let skillIcons4 = document.querySelector(".skillIconImg4");
+skillIcons4.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText4").style.opacity = "1";
+});
+skillIcons4.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText4").style.opacity = "0";
+});
+
+//css 
+let skillIcons5 = document.querySelector(".skillIconImg5");
+skillIcons5.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText5").style.opacity = "1";
+});
+skillIcons5.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText5").style.opacity = "0";
+});
+
+//js 
+let skillIcons6 = document.querySelector(".skillIconImg6");
+skillIcons6.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText6").style.opacity = "1";
+});
+skillIcons6.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText6").style.opacity = "0";
+});
+
+//react
+let skillIcons7 = document.querySelector(".skillIconImg7");
+skillIcons7.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText7").style.opacity = "1";
+});
+skillIcons7.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText7").style.opacity = "0";
+});
+
+//node.js 
+let skillIcons8 = document.querySelector(".skillIconImg8");
+skillIcons8.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText8").style.opacity = "1";
+});
+skillIcons8.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText8").style.opacity = "0";
+});
+
+//git
+let skillIcons9 = document.querySelector(".skillIconImg9");
+skillIcons9.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText9").style.opacity = "1";
+});
+skillIcons9.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText9").style.opacity = "0";
+});
+
+//github
+let skillIcons10 = document.querySelector(".skillIconImg10");
+skillIcons10.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText10").style.opacity = "1";
+});
+skillIcons10.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText10").style.opacity = "0";
+});
+
+//python
+let skillIcons11 = document.querySelector(".skillIconImg11");
+skillIcons11.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText11").style.opacity = "1";
+});
+skillIcons11.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText11").style.opacity = "0";
+});
+
+//networking
+let skillIcons12 = document.querySelector(".skillIconImg12");
+skillIcons12.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText12").style.opacity = "1";
+});
+skillIcons12.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText12").style.opacity = "0";
+});
+
+//SE
+let skillIcons13 = document.querySelector(".skillIconImg13");
+skillIcons13.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText13").style.opacity = "1";
+});
+skillIcons13.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText13").style.opacity = "0";
+});
+
+//SD
+let skillIcons14 = document.querySelector(".skillIconImg14");
+skillIcons14.addEventListener("mouseover", () => {
+    document.querySelector(".skillIconText14").style.opacity = "1";
+});
+skillIcons14.addEventListener("mouseout", () => {
+    document.querySelector(".skillIconText14").style.opacity = "0";
+});
