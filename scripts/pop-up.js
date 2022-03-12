cf01984a77f374b622e36e7346ff4fef345a4bc6
partha7978 <muduliparthasarathi@gmail.com>
2022-03-13 00:02:12 +0530

    popup close fade out animation added

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -23,12 +23,20 @@ setTimeout(() => {
 
 
 btn.addEventListener("click", () => {
-  modal.style.display = "none";
+  modelContent.classList.add("animate__animated", "animate__fadeOutDown");
+  setTimeout(() => {
+    modal.style.display = "none";
+
+  },2000);
 });
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modelContent.classList.add("animate__animated", "animate__fadeOutDown");
+    setTimeout(() => {
+      modal.style.display = "none";
+  
+    },2000);
   }
 }
