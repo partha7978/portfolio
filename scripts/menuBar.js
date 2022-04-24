19a6e1c75844baaefadf7401f5cb51516c8e18b3
partha7978 <muduliparthasarathi@gmail.com>
2022-04-24 19:17:02 +0530

    adding responsiveness to menubar by using JS

--- scripts/menuBar.js
+++ scripts/menuBar.js
@@ -1,36 +1,41 @@
-/* Open when someone clicks on the span element */
-
-// let menu = document.getElementById("menu-ul");
-// let menuBtn = document.getElementsByClassName("hamburger")[0];
-
-// menuBtn.addEventListener("click", () =>  {
-//     if(menu.className === "menu-ul") {
-//         menu.className += " responsive";
-//     } else {
-//         menu.className = "menu-ul";
-//     }
-// });
-
 const windowResize = () => {
   if (window.innerWidth <= 700) {
     document.querySelector(".hamburger").classList.add("hamburger-mobile");
     let btn = document.querySelector(".hamburger-mobile");
     btn.addEventListener("click", () => {
-      document.querySelector(".nav-bar-smallScreen").style.width = "100%";
+      document.querySelector(".nav-bar-smallScreen").style.width = "40%";
     });
   } else {
     document.querySelector(".hamburger").classList.remove("hamburger-mobile");
   }
 };
 
+
+// ! menu bar responsiveness 
+const menuBarResponsive = () => {
+  if (window.innerWidth <= 450) {
+    let btn2 = document.querySelector(".hamburger-mobile");
+    btn2.addEventListener("click", () => {
+       document.querySelector(".nav-bar-smallScreen").style.width = "60%";
+    });
+
+   
+  }
+};
+
 window.addEventListener("resize", () => {
   // Toggle on resizing the window.
   windowResize();
+  menuBarResponsive();
 });
 //toggle when the websie loads 1st in small screen.
 windowResize();
+menuBarResponsive();
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 const closeNav = () => {
   document.querySelector(".nav-bar-smallScreen").style.width = "0%";
 };
+
+
+
