e16dea8f35899e4b6228cb60a02c47f8148e4e10
partha7978 <muduliparthasarathi@gmail.com>
2022-04-24 18:53:22 +0530

    redesigning first page and adding a responsive navbar

--- scripts/menuBar.js
+++ scripts/menuBar.js
@@ -1,9 +1,36 @@
 /* Open when someone clicks on the span element */
-function openNav() {
-  document.querySelector(".nav-bar").style.width = "100%";
-}
+
+// let menu = document.getElementById("menu-ul");
+// let menuBtn = document.getElementsByClassName("hamburger")[0];
+
+// menuBtn.addEventListener("click", () =>  {
+//     if(menu.className === "menu-ul") {
+//         menu.className += " responsive";
+//     } else {
+//         menu.className = "menu-ul";
+//     }
+// });
+
+const windowResize = () => {
+  if (window.innerWidth <= 700) {
+    document.querySelector(".hamburger").classList.add("hamburger-mobile");
+    let btn = document.querySelector(".hamburger-mobile");
+    btn.addEventListener("click", () => {
+      document.querySelector(".nav-bar-smallScreen").style.width = "100%";
+    });
+  } else {
+    document.querySelector(".hamburger").classList.remove("hamburger-mobile");
+  }
+};
+
+window.addEventListener("resize", () => {
+  // Toggle on resizing the window.
+  windowResize();
+});
+//toggle when the websie loads 1st in small screen.
+windowResize();
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.querySelector(".nav-bar").style.width = "0%";
-}
+const closeNav = () => {
+  document.querySelector(".nav-bar-smallScreen").style.width = "0%";
+};
