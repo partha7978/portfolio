5bb6c9e18bc87f5bf63e064267520a5e560de5ee
partha7978 <muduliparthasarathi@gmail.com>
2022-04-25 19:53:03 +0530

    making the first page completely responsive

--- scripts/autoText.js
+++ scripts/autoText.js
@@ -1,5 +1,6 @@
 let typed = new Typed(".gradient", {
-    strings: ["Partha Sarathi Muduli", "Web Developer", "Software Engineer", "Competitive Coder"],
+    // strings: ["Partha Sarathi Muduli", "Web Developer", "Software Engineer", "Competitive Coder"],
+    strings: ["Partha Sarathi Muduli"],
     typeSpeed: 75,
     backSpeed: 75,
     loop: true
