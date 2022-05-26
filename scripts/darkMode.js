e5da489ffc4f6f6911dfebd77a923b2d636d05ea
partha7978 <muduliparthasarathi@gmail.com>
2022-05-26 20:52:18 +0530

    creating a dark mode CSS file for adding dark mode classes

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,11 +1,21 @@
 let checkbox = document.querySelector(".darkModeCheckbox");
+
+let html = document.getElementsByTagName("html");
+//darkModeFunction
+
+
+//darkmode button
 checkbox.addEventListener("click", (e) => {
     if(checkbox.classList.contains('on')) {
         checkbox.setAttribute('aria-checked', 'false');
+        console.log('darkmode off');
     }
     else
     {
+        //!Dark mode ON part
         checkbox.setAttribute('aria-checked', 'true');
+        console.log('darkmode on');
     }
     checkbox.classList.toggle('on');
+   
 })
