fa2ec06728f960b77eb957b703250397562b7252
partha7978 <muduliparthasarathi@gmail.com>
2022-05-31 01:26:39 +0530

    adding shadow to the first page hire me button in DARK MODE

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -3,6 +3,7 @@ let checkbox = document.querySelector(".darkModeCheckbox");
 let html = document.querySelector(".first-page");
 let navBarItem = document.querySelectorAll(".menuItem");
 let firstPageMainHeading = document.querySelector(".animate__bounce");
+let webpageBtn = document.querySelectorAll(".webpage-btn");
 //darkModeFunction
 const darkMode = () => {
     html.classList.toggle('darkMode');
@@ -11,6 +12,9 @@ const darkMode = () => {
         item.classList.toggle('navBarColor');
     });
     firstPageMainHeading.classList.toggle('dark-mode-text');
+    webpageBtn.forEach(item => {
+        item.classList.toggle('dark-mode-btn');
+    });
 }
 
 //darkmode button
