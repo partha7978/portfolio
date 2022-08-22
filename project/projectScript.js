abb6e20872d45ba0f90ebd532a78c4e2849f1c15
partha7978 <muduliparthasarathi@gmail.com>
2022-08-23 00:11:40 +0530

    adding animations on hover in project page quick links

--- project/projectScript.js
+++ project/projectScript.js
@@ -2,6 +2,8 @@ let btn = document.querySelector('.backBtn');
 let icon = document.querySelector(".back-arrow");
 let goBackText = document.querySelector(".goBack-text");
 
+let quickLink = document.querySelectorAll(".project-quick-link");
+
 //! For go back button animation
 btn.addEventListener('mouseover', () => {
     icon.style.transition = "all 1s ease-in";
@@ -12,4 +14,18 @@ btn.addEventListener('mouseover', () => {
 btn.addEventListener('mouseout', () => {
     goBackText.style.transform = 'translateX(-5px)';
     icon.style.display = 'none';
+})
+
+//! for quick link animation
+quickLink.forEach(link => {
+    let i= link.querySelector(".fa-chevron-right");
+    link.addEventListener('mouseover', () => {
+        
+        i.style.transition = "all 0.3s ease-in";
+        i.style.transform = 'rotate(360deg)';
+    })
+    link.addEventListener('mouseout', () => {
+        i.style.transform = 'rotate(0deg)';
+    })
+
 })
