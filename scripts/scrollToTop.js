6de129d50c57f7b55f3de55cbd5491d8c7a23fa2
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-12-06 00:51:16 +0530

    adding scroll to top function in the scroll to top button

---
+++ scripts/scrollToTop.js
@@ -0,0 +1,13 @@
+let handleOnScroll = () => {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  /* or you can also use  document.getElementsByTagName('html')[0].scrollTop; */
+    if (scrollTop > 20) {
+        document.querySelector(".scroll-to-top").style.display = "block";
+    } else {
+        document.querySelector(".scroll-to-top").style.display = "none";
+    }
+};
+
+window.addEventListener("scroll", handleOnScroll);
+document.getElementById("scroll-to-top-btn").addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
